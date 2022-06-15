fd70d002a7fc9f085325f86e6468653f735c8cdb
Bharat1111 <bharatadya1992@gmail.com>
2022-06-15 21:32:49 +0530

    typo

--- server/server.js
+++ server/server.js
@@ -1,34 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
- app.use(express.static('client/build'));
- app.get('*', (req, res) => {
- res.sendFile(path.join(__dirname + '/client/build/index.html'));
- });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "../client/build/index.html"));
+  });
 }
-
 // Routes
-app.use('/api/posts', require('./routes/postRoute'))
-app.use('/api/user', require('./routes/userRouter'))
+app.use("/api/posts", require("./routes/postRoute"));
+app.use("/api/user", require("./routes/userRouter"));
 
 // Connect to MongoDB
-const URI = process.env.URI
+const URI = process.env.URI;
 
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }).then(() => {
-    console.log('Connected to Mongo!');
-})
-.catch((err) => {
-    console.error('Error connecting to Mongo', err);
-});
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Connected to Mongo!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to Mongo", err);
+  });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log('Server is running on port', PORT)
-})
+  console.log("Server is running on port", PORT);
+});
